0ca48bebe41c80bb64381115c3ebaf43ec362ab2
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-09-17 09:31:42 -0400

    displayed all products on page

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const iconCart = document.querySelector('.icon-cart');
 const cartTab = document.querySelector('.cartTab');
 const header = document.querySelector('header');
-const listProduct = document.querySelector('.listProduct')
+const listProductHTML = document.querySelector('.listProduct');
+
+let listProducts = [];
 
 iconCart.addEventListener('click', () => {
   cartTab.classList.toggle('showCart');
   header.classList.toggle('transform');
-  listProduct.classList.toggle('transform');
+  listProductHTML.classList.toggle('transform');
 })
+
+const addDataToHTML = () => {
+  listProductHTML.innerHTML = '';
+  if(listProducts.length > 0) {
+    listProducts.forEach(product => {
+      let newProduct = document.createElement('div');
+      newProduct.classList.add('item');
+      newProduct.innerHTML = `
+        <div class="productImage">
+          <img src="${product.image}" alt="${product.name}">
+        </div>
+        <h2>${product.name}</h2>
+        <div class="price">${product.price}</div>
+        <button class="addCart">
+          Add To Cart
+        </button>
+      `;
+      listProductHTML.appendChild(newProduct);
+    })
+  }
+}
+
+const initApp = () => {
+  fetch('products.json')
+  .then(response => response.json())
+  .then(data => {
+    listProducts = data;
+    addDataToHTML();
+  })
+}
+initApp();
